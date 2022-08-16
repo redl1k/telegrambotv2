9ba39b7067a2cd9d78eca43dc138a0a4248397a9
Andrian Buzdanov <82593138+redl1k@users.noreply.github.com>
2022-08-16 17:10:46 +0300

    Update botTG.py

--- botTG.py
+++ botTG.py
@@ -1,13 +1,12 @@
 import telebot
 from bs4 import BeautifulSoup
 import requests
-bot = telebot.TeleBot('5048860066:AAGrGUWyVGTiJS1lPbtue3ari8bVzKfaPeo')
+bot = telebot.TeleBot('urToken')
 
 help_string = []
 help_string.append("Справка об командах:\n\n")
 help_string.append("/menu - меню с функциями;\n")
 help_string.append("/ineedhelp - обратиться за помощью;\n")
-# help_string.append("/server - присылает статус сервера.")
 
 
 @bot.message_handler(commands=['help'])
@@ -18,7 +17,7 @@ def send_help(message):
 
 @bot.message_handler(commands=['ineedhelp'])
 def send_helper(message):
-    bot.send_message(message.chat.id, 'Обратиться за помощью к нему:\n@RedLikYT')
+    bot.send_message(message.chat.id, 'Обратиться за помощью к нему')
 
 
 @bot.message_handler(commands=['menu'])
@@ -36,56 +35,13 @@ def query_handler(call):
     # то после ответа, клавиатура будет исчезать из чата
     bot.answer_callback_query(callback_query_id=call.id, text='Ok')
     if call.data == '1':
-        answer = 'Начинаем функцию парсинга. \n Ожидайте...'
-        url = 'https://en.wikipedia.org/wiki/Machine_learning'
-        page = requests.get(url)
-
-        # Парсим страницу
-
-        soup = BeautifulSoup(page.text, 'html.parser')
-        events = soup.findAll(res.text, 'html5lib')
-        answer1 = ''
-
-
-
-
-
-#    elif call.data == '2':
-#        answer = 'Вот твой профиль'
-#    elif call.data == '3':
-#        answer = 'Ссылки \n links: \n links2: '
+        answer = 'Ты выбрал 1 кнопку'
+    elif call.data == '2':
+        answer = 'Ты выбрал 2 кнопку'
+   elif call.data == '3':
+        answer = 'Ты выбрал 3 кнопку'
 
     bot.send_message(call.message.chat.id, answer)
-    bot.send_message(call.message.chat.id, answer1)
-
-# def callback_worker(call):
-#     if call.data == '1':
-#         url = 'https://www.afisha.ru/chelyabinsk/schedule_concert/'
-#         page = requests.get(url)
-#
-#         # Парсим страницу
-#
-#         soup = BeautifulSoup(page.text, 'html.parser')
-#         events = soup.findAll('li', class_='SlE6Y _1gSmu')
-#         answer1 = ''
-#         # Собираем необходимые данные со страницы:
-#
-#         for i in events:
-#             event = i.find('section').find('h3').find('a').get_text()
-#             try:
-#                 desc = i.find('section').find('div', class_='').get_text()
-#             except:
-#                 desc = "Нет описания"
-#             date = i.find('section').find('div', class_='_1Jo7v').get_text()
-#
-#             answer1 += event + desc + date + '\n\n'
-#
-#             # Отправляем через бота:
-#         @bot.message_handler(content_types=['text'])
-#         def send_events_chel(message):
-#              bot.send_message(message.chat.id, answer1)
-#
-#
 
 
 @bot.message_handler(content_types=['text'])
